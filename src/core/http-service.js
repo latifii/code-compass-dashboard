7dfb87c9cb4f5bb2ee48b6fafb0629ed21ff253c
latifii <latifii68@gmail.com>
2024-09-27 22:50:04 +0330

    edit env

--- src/core/http-service.js
+++ src/core/http-service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.VITE_bASE_URL;
+const BASE_URL = import.meta.env.VITE_BASE_URL;
 
 export const httpService = axios.create({
   baseURL: BASE_URL,
